3651600872a1ca2c7cbf8e1bce054a71d5dfa9d7
Vozmo <vozmo7@gmail.com>
2025-05-19 23:22:56 +0700

    Remove footer link section, improvised the contact links section, and add condition to redirect links~

--- js/head.js
+++ js/head.js
@@ -1,10 +1,12 @@
+// Only run redirect if NOT on localhost or 127.0.0.1
+if (window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1") {
+    // Redirect to HTTPS if not already
+    if (location.protocol !== 'https:') {
+        location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
 
-// // Redirect to HTTPS if not already
-// if (location.protocol !== 'https:') {
-//     location.replace(`https:${location.href.substring(location.protocol.length)}`);
-// }
-
-// // Redirect non-www to www (after HTTPS is ensured)
-// else if (window.location.hostname === "lolife.xyz") {
-//     window.location.href = "https://www.lolife.xyz" + window.location.pathname + window.location.search;
-// }
+    // Redirect non-www to www (after HTTPS is ensured)
+    else if (window.location.hostname === "lolife.xyz") {
+        window.location.href = "https://www.lolife.xyz" + window.location.pathname + window.location.search;
+    }
+}
